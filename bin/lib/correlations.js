9864cbd985ffcbd3aaf59d17712bbd00a3c5468b
Sean Tracey <seanmtracey@gmail.com>
2017-07-07 11:37:43 +0100

    Minimal correlations API interface

--- bin/lib/correlations.js
+++ bin/lib/correlations.js
@@ -1,14 +1,65 @@
 const debug = require('debug')('bin:lib:correlations');
+const fetch = require('node-fetch');
+
+const CORRELATION_SERVICE_HOST = process.env.CORRELATION_SERVICE_HOST;
+const CORRELATIONS_SERVICE_TOKEN = process.env.CORRELATIONS_SERVICE_TOKEN;
+
+function getAllOfTheIslandsInTheCorrelationsService(){
+
+	return fetch(`https://${CORRELATION_SERVICE_HOST}/allIslands?token=${CORRELATIONS_SERVICE_TOKEN}`)
+		.then(res => {
+			if(res.ok){
+				return res.json();
+			} else {
+				throw res;
+			}
+		})
+		.catch(err => {
+			debug(err); //Log the error here, catch it in the application
+			throw err;
+		})
+	;
+
+}
 
 function getListOfPeopleOnAPersonsIsland(personName){
+
+	return fetch(`https://${CORRELATION_SERVICE_HOST}/islandOf/people:${ encodeURIComponent( personName ) }?token=${CORRELATIONS_SERVICE_TOKEN}`)
+		.then(res => {
+			if(res.ok){
+				return res.json();
+			} else {
+				throw res;
+			}
+		})
+		.catch(err => {
+			debug(err); //Log the error here, catch it in the application
+			throw err;
+		})
+	;
 
 }
 
 function getListOfPeopleByDistances(personName){
 
+	return fetch(`https://${CORRELATION_SERVICE_HOST}/calcChainLengthsFrom/people:${ encodeURIComponent( personName ) }?token=${CORRELATIONS_SERVICE_TOKEN}`)
+		.then(res => {
+			if(res.ok){
+				return res.json();
+			} else {
+				throw res;
+			}
+		})
+		.catch(err => {
+			debug(err); //Log the error here, catch it in the application
+			throw err;
+		})
+	;
+
 }
 
 module.exports = {
+	allIslands : getAllOfTheIslandsInTheCorrelationsService,
 	islandOf : getListOfPeopleOnAPersonsIsland,
 	calcChainLengthsFrom : getListOfPeopleByDistances
 };
--- bin/lib/game.js
+++ bin/lib/game.js
@@ -1,9 +1,38 @@
 const debug = require('debug')('bin:lib:correlations');
+const uuid = require('uuid').v4;
 
 const runningGames = {};
 const highScores = [];
 
+/* 
+Game class
+
+UUID = uuid for this game
+player = userUUID
+state = What is the current state of play?
+	- new - Game has not yet been started
+	- current - game is in progress
+	- finished - game has been completed
+distance - the furthest distance achieved from the original (seed) person on the island
+seedPerson - the person whose island we're starting on.
+*/
+
+Class Game(){
+	constructor(userUUID) {
+		this.UUID = uuid();
+		this.player = userUUID;
+		this.state = 'new';
+		this.distance = 0;
+		this.seedPerson = 'Donald Trump'
+	}
+}
+
 function startANewGame(userUUID){
+
+	const newGame = new Game(userUUID);
+	runningGames[newGame.UUID] = newGame;
+
+	return newGame.UUID;
 
 }
 
