e495a19ddf1629ef570e085f7b7e241f615f3dad
Lily2point0 <lily.madar@ft.com>
2017-07-14 10:32:12 +0100

    try saved answers

--- routes/voice.js
+++ routes/voice.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const games = require('../bin/lib/game');
+let expectedAnswers = [];
 
 router.post('/googlehome', (req, res) => {
 	const USER_INPUT = req.body.result.resolvedQuery;
@@ -11,6 +12,73 @@ router.post('/googlehome', (req, res) => {
 
 	res.setHeader('Content-Type', 'application/json');
 
+	// games.check(SESSION)
+	// .then(gameIsInProgress => {
+	// 	if(gameIsInProgress){
+	// 		console.log('PROGRESS');
+	// 		return games.question(SESSION);
+	// 	} else {
+	// 		return games.new(SESSION)
+	// 			.then(gameUUID => {
+	// 				return gameUUID;
+	// 			})
+	// 			.then(gameUUID => games.question(gameUUID))
+	// 		;
+	// 	}
+	// })
+	// .then(data => {
+	// 	if(data.limitReached === true){
+	// 		answer = 'winner';
+	// 		res.send(JSON.stringify({'speech': answer, 'displayText': answer}));
+	// 	} else {
+	// 		const preparedData = {};
+
+	// 		preparedData.seed = {
+	// 			value : data.seed,
+	// 			printValue : data.seed.replace('people:', '')
+	// 		};
+
+	// 		preparedData.options = {};
+
+	// 		Object.keys(data.options).forEach(key => {
+	// 			preparedData.options[key] = {
+	// 				value : data.options[key],
+	// 				printValue : data.options[key].replace('people:', '')
+	// 			};
+	// 		});
+
+	// 		formatQuestion(preparedData, ans => {
+	// 			res.send(JSON.stringify({'speech': ans, 'displayText': ans}));
+	// 		});
+	// 	}
+
+	// });
+
+	switch(USER_INPUT.toLowerCase()) {
+		case 'start':
+			return startGame(SESSION, ans => {
+				res.send(JSON.stringify({'speech': 'start' + ans, 'displayText': ans}));
+			})
+			// return getQuestion(USER_INPUT, ans => {
+			// 	res.send(JSON.stringify({'speech': ans, 'displayText': ans}));
+			// });
+		break;
+
+		case expectedAnswers[0]:
+		case expectedAnswers[1]:
+		case expectedAnswers[2]:
+			answer = 'You said' + USER_INPUT;
+		break;
+
+		default:
+			answer = 'Sorry I didn\'t quite catch that...'
+	}
+
+	res.send(JSON.stringify({'speech': answer, 'displayText': answer}));
+
+});
+
+function startGame(session, callback) {
 	games.check(SESSION)
 	.then(gameIsInProgress => {
 		if(gameIsInProgress){
@@ -28,7 +96,8 @@ router.post('/googlehome', (req, res) => {
 	.then(data => {
 		if(data.limitReached === true){
 			answer = 'winner';
-			res.send(JSON.stringify({'speech': answer, 'displayText': answer}));
+			// res.send(JSON.stringify({'speech': answer, 'displayText': answer}));
+			callback(answer);
 		} else {
 			const preparedData = {};
 
@@ -47,31 +116,19 @@ router.post('/googlehome', (req, res) => {
 			});
 
 			formatQuestion(preparedData, ans => {
-				res.send(JSON.stringify({'speech': ans, 'displayText': ans}));
+				// res.send(JSON.stringify({'speech': ans, 'displayText': ans}));
+				callback(ans);
 			});
 		}
-
 	});
-
-	// switch(USER_INPUT.toLowerCase()) {
-	// 	case 'start':
-	// 		return getQuestion(USER_INPUT, ans => {
-	// 			res.send(JSON.stringify({'speech': ans, 'displayText': ans}));
-	// 		});
-	// 	break;
-
-	// 	default:
-	// 		answer = 'Sorry I didn\'t quite catch that...'
-	// }
-
-	
-
-});
+}
 
 function formatQuestion(options, callback) {
 	let answerFormat = 'Who was recently mentioned in an article with ' + options.seed.printValue + '?\n';
+	expectedAnswers = [];
 	Object.keys(options.options).forEach(key => {
 		answerFormat += ' - ' + options.options[key].printValue;
+		expectedAnswers.push(options.options[key].printValue);
 	});
 
 	callback(answerFormat);
