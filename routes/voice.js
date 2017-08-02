02adcdc6253309d6e4f20875824a030ab3d7d255
Lily2point0 <lily.madar@ft.com>
2017-08-02 16:12:18 +0100

    readd debug info

--- routes/voice.js
+++ routes/voice.js
@@ -26,10 +26,10 @@ const returnQuestion = app => {
 
 const matchAnswer = app => {
 	console.log('>>>>>>>>>>>>>>MATCH0', app.body_.result.resolvedQuery);
-	// let USER_INPUT = app.body_.result.resolvedQuery;
-	// const SESSION = app.body_.sessionId;
+	let USER_INPUT = app.body_.result.resolvedQuery;
+	const SESSION = app.body_.sessionId;
 
-	// console.log('MATCH:::', USER_INPUT, SESSION);
+	console.log('MATCH:::', USER_INPUT, SESSION);
 
 	// getExpectedAnswers(SESSION)
 	// .then(answers => {
