ea544a7ae684716d673860dae3974f3d61396719
Lily2point0 <lily.madar@ft.com>
2017-10-31 17:02:40 +0000

    revert debug

--- bin/lib/log-to-spoor.js
+++ bin/lib/log-to-spoor.js
@@ -7,7 +7,7 @@ module.exports = function(data){
 		return Promise.resolve({'status' : 'Failed. Invalid data passed.'});
 	}
 
-	console.log('Sending data to spoor', data);
+	debug('Sending data to spoor', data);
 
 	return fetch('https://spoor-api.ft.com/ingest', {
 			method: 'POST',
