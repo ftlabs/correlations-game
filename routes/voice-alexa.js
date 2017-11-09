bdc7eae29dc7043e01eae2474d2c6cc731543daa
Cale Tilford <cale.tilford@ft.com>
2017-11-09 11:40:35 +0000

    Add start game intents

--- routes/voice-alexa.js
+++ routes/voice-alexa.js
@@ -6,12 +6,46 @@ const games = require('../bin/lib/game');
 const responses = require('../responses/content');
 const Alexa = require('alexa-sdk');
 
-const handlers = {
-    'HelloWorldIntent': function () {
-        var guessNum = parseInt(this.event.request.intent.slots.number.value);
-        this.emit(':tell', `Hello, you guessed ${guessNum}!`);
+const GAME_STATES = {
+    START: '_STARTMODE',
+    HELP: '_HELPMODE'
+};
+
+const newSessionHandlers = {
+    'LaunchRequest': function () {
+        this.handler.state = GAME_STATES.START;
+        this.emitWithState('WelcomeGame', true);
+    },
+    'AMAZON.StartOverIntent': function () {
+        this.handler.state = GAME_STATES.START;
+        this.emitWithState('StartGame', true);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.handler.state = GAME_STATES.HELP;
+        this.emitWithState('helpTheUser', true);
+    },
+    'Unhandled': function () {
+        const speechOutput = 'Say start to start a new game.';
+        this.response.speak(speechOutput).listen(speechOutput);
+        this.emit(':responseReady');
     }
 };
+
+const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
+    'WelcomeGame': function () {
+        this.response.speak('Let’s play! Say “start” when ready or help for more instructions.');
+        this.emit(':responseReady');
+    },
+    'StartGame': function() {
+        const sessionId = this.event.session.sessionId;
+
+        games.check(sessionId);
+        games.new(sessionId);
+
+        this.response.speak('Thanks for playing!');
+        this.emit(':responseReady');
+    }
+});
 
 router.post('/', (request, response) => {
 
@@ -27,7 +61,7 @@ router.post('/', (request, response) => {
     };
 
     const alexa = Alexa.handler(request.body, context);    
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(newSessionHandlers, startStateHandlers);
     alexa.execute();
 });
 
