964d23c083efa14534eed1a7f430ca0467a62b26
Cale Tilford <cale.tilford@ft.com>
2017-11-23 17:01:12 +0000

    Add LanchRequest example

--- tests/test.js
+++ tests/test.js
@@ -13,4 +13,36 @@ const alexaSkill = require('../routes/voice-alexa.js');
         .then(answer => {
             console.log(answer);
         });
+
+    Send a request 
+
+    helper.sendRequest(request, alexaSkill.handler)
+        .then(response => {
+            console.log(response);
+        })
 */
+
+const launchRequest = {
+    "session": {
+      "attributes": {},
+      "sessionId": "1234",
+      "application": {
+        "applicationId": "amzn1.echo-sdk-ams.app.123"
+      },
+      "user": {
+        "userId": "test_user"
+      },
+      "new": true
+    },
+    "request": {
+      "type": "LaunchRequest",
+      "locale": "en-GB",
+      "requestId": "request_id_123",
+      "timestamp": 1449829632387
+    }
+  }
+
+helper.sendRequest(launchRequest, alexaSkill.handler)
+    .then(response => {
+        console.log(response);
+    })
