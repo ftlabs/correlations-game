109330543141282ce0711e96ef1269b6fc9dbb7d
Cale Tilford <cale.tilford@ft.com>
2017-11-23 11:37:18 +0000

    Get correct answer for question from correlations service

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 require('dotenv').load();
 
+const correlations_service = require('../bin/lib/correlations');
+
 const conversation = require('alexa-conversation');
 const app = require('../routes/voice-alexa.js');
 
@@ -9,6 +11,53 @@ const opts = {
     app: app
 };
 
+function getCorrectAnswer(personX, people) {
+    return correlations_service.calcChainLengthsFrom(`people:${personX}`)
+        .then(result => {
+            let directConnections;
+            for (let r of result) {
+                if (r.links === 1) directConnections = r.entities;
+            }
+            directConnections = directConnections.map(p => p.replace('people:', ''));
+            for (const p of people) {
+                if (directConnections.includes(p)) return p;
+            }
+        });
+}
+
+function getPeopleFromQuestion(question) {
+    const reX = /Question [0-9]+\. (.*?) was mentioned/;
+    const resultX = reX.exec(question);
+
+    const reOne = /one\) (.*?)\./;
+    const resultOne = reOne.exec(question);
+
+    const reTwo = /two\) (.*?)\./;
+    const resultTwo = reTwo.exec(question);
+
+    const reThree = /three\) (.*?)\./;
+    const resultThree = reThree.exec(question);
+    
+    return {
+        personX: resultX[1],
+        people: [
+            resultOne[1],
+            resultTwo[1],
+            resultThree[1]
+        ]
+    };
+}
+
+/* 
+    How to get a correct answer from the correlations service
+
+    const questionExample = `Question 1. Robert Mugabe was mentioned in a recent article with which one of the following people? one) Morgan Tsvangirai. two) Sean Hannity. three) Roy Moore.`;
+    const extractedPeople = getPeopleFromQuestion(questionExample);
+    getCorrectAnswer(extractedPeople.personX, extractedPeople.people)
+        .then(answer => {
+            console.log(answer);
+        });
+*/
 
 conversation(opts)
     .userSays('LaunchRequest')
