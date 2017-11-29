1252a6250b97d7e6ff4ac52093c005712d0562e4
Cale Tilford <cale.tilford@ft.com>
2017-11-29 13:57:42 +0000

    Generate all paths from tree

--- tests/alexa-tester.js
+++ tests/alexa-tester.js
@@ -6,10 +6,44 @@ const alexaSkill = require('../routes/voice-alexa.js');
 
 const exampleModel = "tests/models/example.json";
 
+function getSimpleNode(node) {
+    let simpleNode = {
+        type: node.requestType
+    };
+    if (node.name) {
+        simpleNode.name = node.name
+    }
+    if (node.slots) {
+        simpleNode.slots = node.slots;
+    }
+    return simpleNode;
+}
+
+function traverseTree(node) {
+    let paths = [];
+    traverse(node, [], paths);
+    return paths;
+}
+
+function traverse(node, path, paths) {
+    if (node.children) {
+        for (let childNode of node.children) {
+            let newPath = path.slice();
+            newPath.push(getSimpleNode(node));
+            traverse(childNode, newPath, paths);
+        }
+    } else {
+        path.push(getSimpleNode(node));
+        paths.push(path);
+    }
+}
+
 async function testSkillTree(skillTreeJson, handler) {
     const json = await helper.getInteractionModelFromJSON(skillTreeJson);
     const model = JSON.parse(json);
 
+    const paths = traverseTree(model);   
+    
     const info = {
         applicationId: 'amzn1.echo-sdk-ams.app.123',
         userId: 'test-user',
@@ -22,7 +56,9 @@ async function testSkillTree(skillTreeJson, handler) {
     };
     const attributes = {};
 
-    return testNode(model, handler, info, session, attributes);
+    // return testNode(model, handler, info, session, attributes);
+
+    return paths;
 }
 
 async function testNode(node, handler, info, session, attributes) {    
@@ -30,6 +66,7 @@ async function testNode(node, handler, info, session, attributes) {
     const request = {
         type: node.requestType
     };
+
     if (node.name) {
         request.name = node.name;
     }
@@ -41,7 +78,18 @@ async function testNode(node, handler, info, session, attributes) {
 
     const newRequest = helper.buildRequest(info, session, attributes, request);
     
-    return helper.sendRequest(newRequest, handler);
+    const response = await helper.sendRequest(newRequest, handler);
+    const updatedAttributes = response.sessionAttributes;
+
+    console.log(node.requestType, node.name);
+
+    if (response.response.shouldEndSession) {
+        return response;
+    } else {
+        return Promise.all(node.children.map(n => {
+            return testNode(n, handler, info, session, updatedAttributes);
+        }));
+    }
 };
 
 testSkillTree(exampleModel, alexaSkill.handler)
