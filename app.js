94a54c821d8831263155214ca6f0aaad8cfe734c
Jamie.Brown <jamiebrown201@hotmail.com>
2018-06-13 16:26:16 +0100

    add correct status syntax

--- app.js
+++ app.js
@@ -1,60 +1,59 @@
-const express = require('express');
-const path = 	require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const routeAuth = require('./bin/middleware/route-auth');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const routeAuth = require("./bin/middleware/route-auth");
 
 const app = express();
 
-const userUUIDMiddleware = require('./bin/middleware/user-uuid');
+const userUUIDMiddleware = require("./bin/middleware/user-uuid");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));	
+app.use(logger("dev"));
 
-//Use verify to append rawbody attribute to request before parse. 
+//Use verify to append rawbody attribute to request before parse.
 //(Used by alexa route guard)
 app.use(
-	bodyParser.json({
-		verify: function (req, res, buf) {
-			req.rawBody = buf
-		}
-	}
-	))
+  bodyParser.json({
+    verify: function(req, res, buf) {
+      req.rawBody = buf;
+    }
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(userUUIDMiddleware);
 
-app.use('/', require('./routes/index'));
-app.use('/voice', routeAuth, require('./routes/voice'));
-app.use('/interface', require('./routes/interface'));
-app.use('/alexa', routeAuth, require('./routes/voice-alexa').router);
-
+app.use("/", require("./routes/index"));
+app.use("/voice", routeAuth, require("./routes/voice"));
+app.use("/interface", require("./routes/interface"));
+app.use("/alexa", routeAuth, require("./routes/voice-alexa").router);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
--- bin/middleware/route-auth.js
+++ bin/middleware/route-auth.js
@@ -19,50 +19,32 @@ module.exports = (req, res, next) => {
     return next();
   }
 
-  console.log(
-    "+++++++++++++++++++++++++++++++++++++req.get('signaturecertchainurl')",
-    req.get("signaturecertchainurl")
-  );
-  console.log(
-    "+++++++++++++++++++++++++++++++++++++req.get('signature')",
-    req.get("signature")
-  );
-
-  console.log("+++++++++++++++++++creds", creds);
-  console.log("+++++++++++++++++++!creds", !creds);
-
   //First check for amazon header params, if they exists, check the certificate and verify the request.
   if (req.get("signaturecertchainurl") && req.get("signature")) {
-    console.log("+++++++++++++++++++++++++++++++++++++Got INSIDE THE IF BLOCK");
     verifier(
       req.get("signaturecertchainurl"),
       req.get("signature"),
       req.rawBody.toString(),
       err => {
         if (err) {
-          console.log("+++++++++++++++++ERROR");
-          res.status = 400;
-          res.json(errResponse);
+          res.status(400).json(errResponse);
         } else {
-          console.log("+++++++++++++++++SUCCESS");
           next();
         }
       }
     );
   }
-
   //Else check for Basic Auth
-  //   else if (!creds) {
-  //     res.statusCode = 401;
-  //     res.setHeader("WWW-Authenticate", 'Basic realm="example"');
-  //     res.end();
-  //   } else if (
-  //     creds.name === process.env.BASIC_AUTH_USERNAME &&
-  //     creds.pass === process.env.BASIC_AUTH_PASSWORD
-  //   ) {
-  //     next();
-  //   }
-  else {
+  else if (!creds) {
+    res.statusCode = 401;
+    res.setHeader("WWW-Authenticate", 'Basic realm="example"');
+    res.end();
+  } else if (
+    creds.name === process.env.BASIC_AUTH_USERNAME &&
+    creds.pass === process.env.BASIC_AUTH_PASSWORD
+  ) {
+    next();
+  } else {
     res.status(400).json(errResponse);
   }
 };
